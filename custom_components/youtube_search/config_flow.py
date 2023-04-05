6b183b12b84b58378d18c156a5a4b5bc074532aa
hokiebrian <64511748+hokiebrian@users.noreply.github.com>
2023-04-04 20:58:21 -0400

    Code Updates, Error handling, API Key validation

--- custom_components/youtube_search/config_flow.py
+++ custom_components/youtube_search/config_flow.py
@@ -1,5 +1,6 @@
 from homeassistant import config_entries
 from .const import DOMAIN
+import aiohttp
 
 class YouTubeFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -8,12 +9,27 @@ class YouTubeFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         errors = {}
         if user_input is not None:
-            await self.async_set_unique_id(user_input["api_key"])
-            self._abort_if_unique_id_configured()
-            return self.async_create_entry(
-                title="YouTube Search",
-                data={"api_key": user_input["api_key"]},
-            )
+            # Validate the user input against the expected schema
+            try:
+                self.schema(user_input)
+            except vol.Invalid as e:
+                errors[e.path[0]] = str(e)
+
+            # Test the YouTube API with the user's API key
+            api_key = user_input.get("api_key")
+            success = await self.test_youtube_api_key(api_key)
+
+            if not success:
+                errors["base"] = "Failed to connect to YouTube API with the provided API key."
+
+            if not errors:
+                # The user input is valid and the API test passed, create a new config entry
+                await self.async_set_unique_id(user_input["api_key"][:7])
+                self._abort_if_unique_id_configured()
+                return self.async_create_entry(
+                    title="YouTube Search",
+                    data={"api_key": user_input["api_key"]},
+                )
 
         return self.async_show_form(
             step_id="user",
@@ -21,9 +37,23 @@ class YouTubeFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             errors=errors,
         )
 
+    async def test_youtube_api_key(self, api_key):
+        """Test the connection to the YouTube API using the provided API key."""
+        url = f"https://www.googleapis.com/youtube/v3/videos?id=Ks-_Mh1QhMc&part=id,snippet&key={api_key}"
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                response_data = await response.json()
+
+                if response.status != 200:
+                    return False
+                elif "error" in response_data:
+                    return False
+                else:
+                    return True
+
     @property
     def schema(self):
         from homeassistant.helpers import config_validation as cv
         import voluptuous as vol
-
         return vol.Schema({vol.Required("api_key"): cv.string})
--- custom_components/youtube_search/sensor.py
+++ custom_components/youtube_search/sensor.py
@@ -1,14 +1,17 @@
 import asyncio
 import json
+import logging
 from aiohttp import ClientSession
 from homeassistant.helpers.entity import Entity
 from homeassistant.const import CONF_API_KEY
 from .const import DOMAIN
 
+_LOGGER = logging.getLogger(__name__)
+
 async def async_setup_entry(hass, config_entry, async_add_entities):
     sensor = YouTubeSearchSensor(config_entry)
     async_add_entities([sensor], True)
-
+    
 class YouTubeSearchSensor(Entity):
     def __init__(self, config_entry):
         self.api_key = config_entry.data[CONF_API_KEY]
@@ -19,7 +22,7 @@ class YouTubeSearchSensor(Entity):
         self.hass.services.async_register(DOMAIN, "search_video", self.search_video)
 
     async def async_will_remove_from_hass(self):
-        self.hass.services.async_unregister(DOMAIN, "search_video")
+        self.hass.services.async_remove(DOMAIN, "search_video")
 
     async def search_video(self, call):
         video_title = call.data.get("video_title")
@@ -29,11 +32,27 @@ class YouTubeSearchSensor(Entity):
         async with ClientSession() as session:
             async with session.get(url_search) as response_search:
                 search_data = await response_search.json()
+                if 'items' not in search_data:
+                    self._state = "No Results Found"
+                    self._attributes = {}
+                    self.async_write_ha_state()
+                    self._LOGGER.error(f"Search Error{video_title}")
+                    return
+
                 video_id = search_data['items'][0]['id']['videoId']
+                self._LOGGER.debug(f"Search {video_title} results {video_id}")
 
             async with session.get(url_video_details.format(video_id, self.api_key)) as response_video_details:
                 video_details_data = await response_video_details.json()
+                if 'items' not in video_details_data:
+                    self._state = None
+                    self._attributes = {}
+                    self.async_write_ha_state()
+                    self._LOGGER.error(f"No Data Found for {video_id}")
+                    return
+
                 response_video_details = video_details_data['items'][0]
+                self._LOGGER.debug(f"Search {video_id} results {response_video_details}")
 
             self._state = video_id
             self._attributes = response_video_details
@@ -49,7 +68,7 @@ class YouTubeSearchSensor(Entity):
     
     @property
     def unique_id(self):
-        return "YouTubeSearch2023"
+        return f"YouTubeSearch{self.api_key[:7]}"
 
     @property
     def extra_state_attributes(self):
